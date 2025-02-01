52e15aff1f9428aec710edf0c06ec6de75970433
Ubuntu <azureuser@eventrum-selenium.3arzqvrkre1e5ntacn0fnqdz5e.bx.internal.cloudapp.net>
2025-02-01 06:45:16 +0000

    temp

--- api_client.py
+++ api_client.py
@@ -8,28 +8,31 @@ class ApiClient:
     @classmethod
     async def add_articles(cls, genre, results):
         # tasks = [cls.add_article(genre, result) for result in results]
-        tasks = cls.add_article(genre, results[0])
-        await asyncio.gather(tasks)
-        
-    @classmethod
-    async def add_article(cls, genre, result):
-        data = {
-            "sourceId" : result["sourceId"],
-            "sourceName" : result["sourceName"],
-            "author" : result["author"],
-            "title" : result["title"],
-            "description" : result["description"],
-            "url" : result["url"],
-            "urlToImage" : result["urlToImage"],
-            "publishedAt" : result["publishedAt"],
-            "content" : result["content"],
-            "paragraphs" : result["paragraphs"]
-        }
+        datas = [cls.toJson(result) for result in results]
         try:
-            response = await requests.post(f"{cls.BASE_URL}/articles/{genre}", json=data)
+            response = await requests.post(f"{cls.BASE_URL}/articles/{genre}", json=datas)
             if response.status_code == 200:
                 print("Article added successfully:", response.json())
             else:
                 print(f"Failed to add article. Status code: {response.status_code}")
         except requests.RequestException as e:
             print(f"Error connecting to API: {e}")
+
+        
+    @classmethod
+    def toJson(cls, result):
+        data = {
+            "sourceId" : result.get("sourceId"),
+            "sourceName" : result.get("sourceName"),
+            "author" : result.get("author"),
+            "title" : result.get("title"),
+            "description" : result.get("description"),
+            "url" : result.get("url"),
+            "urlToImage" : result.get("urlToImage"),
+            "publishedAt" : result.get("publishedAt"),
+            "content" : result.get("content"),
+            "paragraphs" : result.get("paragraphs"),
+            "videos" : result.get("videos"),
+            "images" : result.get("images")
+        }
+        return data
